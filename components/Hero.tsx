45df058a264d1b62481034a32319110c2c6ad5cc
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-12 02:26:01 +0300

    add globals.css and config for next

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from "next/image"
+
+const MyComponent = () => {
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Найти, забронировать и арендовать автомобиль — ЭТО ПРОСТО!
+                </h1>
+                <p className="hero__subtitle">
+                    Наш сервис поможет Вам забронировать понравившейся Вам автомобиль!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default MyComponent;
