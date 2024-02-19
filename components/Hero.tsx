1d460b2ec399f2e238b5659e2109540be38b6508
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-19 03:26:55 +0300

    add Footer and constants

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,18 +9,21 @@ const MyComponent = () => {
     }
     return (
         <div className="hero">
-            <div className="flex-1 pt-36 padding-x">
+            <div className="flex-1 pt-36 padding-x flex-col">
                 <h1 className="hero__title">
                     Найти, забронировать и арендовать автомобиль — ЭТО ПРОСТО!
                 </h1>
                 <p className="hero__subtitle">
                     Наш сервис поможет Вам забронировать понравившейся Вам автомобиль!
                 </p>
-                <CustomButton
-                    title="Забронировать машину"
-                    containerStyles="bg-primary-blue text-white rounded-full mt-10 self-center"
-                    handleClick={handleScroll}
-                />
+                <div className="border-2">
+                    <CustomButton
+                        title="Забронировать машину"
+                        containerStyles="bg-primary-blue text-white rounded-full mt-10 self-center"
+                        handleClick={handleScroll}
+                    />
+                </div>
+
             </div>
             <div className="hero__image-container">
                 <div className="hero__image">
