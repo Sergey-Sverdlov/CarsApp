32748c28947df66cda4babababd38c407fb412ae
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-25 04:39:12 +0300

    add price and model for CarCard

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,17 +1,29 @@
 "use client"
+
 import React from 'react';
-import { useState} from "react";
+import {useState} from "react";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 interface CarCardProps {
     car: CarCardProps
 }
-const CarCard:React.FC<CarCardProps> = ({car}:CarCardProps) => {
+
+const CarCard: React.FC<CarCardProps> = ({car}: CarCardProps) => {
     console.log(car)
+    const {city_mpg, year, make, model, transmission, drive} = car
     return (
-        <div>
-            <h1>dsf</h1>
+        <div className="car-card">
+            <div className="car-card__content">
+                <h2 className="car-card__content-title">
+                    {make} {model}
+                </h2>
+            </div>
+            <p>
+                <span>
+                    Стоимость
+                </span>
+            </p>
         </div>
     );
 };
