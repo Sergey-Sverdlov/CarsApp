7a85b13ef590e814947e26e2dff0921e00eb873e
Serega <61983829+Sergey-Sverdlov@users.noreply.github.com>
2024-02-26 01:51:29 +0300

    add function calculate price

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,14 +4,15 @@ import React from 'react';
 import {useState} from "react";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
+import {calculateCarRent} from "../utils/index";
 
 interface CarCardProps {
     car: CarCardProps
 }
 
 const CarCard: React.FC<CarCardProps> = ({car}: CarCardProps) => {
-    console.log(car)
     const {city_mpg, year, make, model, transmission, drive} = car
+    const carRent = calculateCarRent(city_mpg, year)
     return (
         <div className="car-card">
             <div className="car-card__content">
@@ -19,11 +20,46 @@ const CarCard: React.FC<CarCardProps> = ({car}: CarCardProps) => {
                     {make} {model}
                 </h2>
             </div>
-            <p>
-                <span>
-                    Стоимость
+            <p className="flex mt-6 text-[32px] font-extrabold ">
+                <span className="self-start text-[14px] font-semibold ">
+                    $
+                </span>
+                {carRent}
+                <span className="self-end text-[14px] font-medium">
+                    /day
                 </span>
             </p>
+
+            <div className="relative w-full h-40 my-3 object-contain">
+                <Image src="/hero.png" alt="car Image"
+                       fill priority className="object-contain"/>
+            </div>
+            <div className="relative flex w-full mt-2">
+                <div className="flex group-hover:invisible w-full justify-between
+                text-gray-500">
+                    <div className="flex flex-col justify-center items-center gap-2">
+                        <Image src="/steering-wheel.svg" alt="steering wheel"
+                               width={20} height={20}/>
+                        <p className="text-[14px]">
+                            {transmission === 'a' ? "АКПП" : "МКПП"}
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-2">
+                        <Image src="/tire.svg" alt="steering wheel"
+                               width={20} height={20}/>
+                        <p className="text-[14px]">
+                            {drive.toUpperCase() === 'FWD' ? "Полный привод" : "Передний привод"}
+                        </p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-2">
+                        <Image src="/gas.svg" alt="steering wheel"
+                               width={20} height={20}/>
+                        <p className="text-[14px]">
+                            {Math.trunc(city_mpg / 2)} л / 100км
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
